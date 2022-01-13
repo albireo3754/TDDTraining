ad21c52a125d5a0aa20ab13db997c6e50e385fcc
starpro123 <starpro123@naver.com>
2022-01-14 02:21:37 +0900

    8장: 추상메소드와 팩토리메소드를 활용해서 중복을 제거함

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,5 +1,14 @@
-public class Money {
+abstract class Money {
+    abstract Money times(int multiplier);
     protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
+    }
 
     Money(int amount) {
         this.amount = amount;
