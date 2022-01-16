29ba6064b30feda56d767a182b06d9f4c6d9a3cf
starpro123 <starpro123@naver.com>
2022-01-17 02:07:00 +0900

    12장: 테스트를 분리하였고, 더 발전해서 새로운 메타포를 추가하였다.

---
+++ src/main/java/Bank.java
@@ -0,0 +1,5 @@
+public class Bank {
+    Money reduce(Expression sum, String usd) {
+        return Money.dollar(10);
+    }
+}
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression{
     protected String currency;
     protected int amount;
 
@@ -30,5 +30,9 @@ class Money {
 
     String currency() {
         return currency;
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
