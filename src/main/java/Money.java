7b45daca9a6016d7f41cdd28354aa3a45bad6e60
starpro123 <starpro123@naver.com>
2022-01-19 02:06:44 +0900

    14장: 필요할거라고 생각한 인자를 추가하고, 테스트의 중복을 제거하기 위해 노력했다.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -20,7 +20,15 @@ class Money implements Expression{
     }
 
     public Money reduce(String to) {
-        return this;
+        int rate = (currency.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
+        return new Money(amount / rate, to);
+    }
+
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 
     Money(int amount, String currency) {
--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -52,6 +52,18 @@ public class DollarTest {
         assertEquals(Money.dollar(7), result);
         Money result2 = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result2);
+    }
 
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 }
