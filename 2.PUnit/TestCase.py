05bc92b19e945414aa5b8b72e3885f3c9500dce2
starpro123 <starpro123@naver.com>
2022-02-24 00:15:40 +0900

    Log기능과 tearDown을 추가하기

--- 2.PUnit/TestCase.py
+++ 2.PUnit/TestCase.py
@@ -1,15 +1,20 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+        self.log = ""
     
     def run(self):
         self.setUp()
         # 파이썬의 장점 클래스 이름이나 메서드 이름을 함수처럼 다룰 수 있다.
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
     def testMethod(self):
         pass
 
     def setUp(self):
+        pass
+
+    def tearDown(self):
         pass
--- 2.PUnit/WasRun.py
+++ 2.PUnit/WasRun.py
@@ -2,13 +2,14 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)        
 
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
-    def run(self):
+    def testMethod(self):
         self.wasRun = 1
-        self.wasSetUp = None
+        self.log = self.log + "testMethod "
+    
+    def tearDown(self):
+        self.log += "tearDown"
