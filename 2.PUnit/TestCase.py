6a1376dc35eae5a8e61c0768e555f52cffd72dd5
starpro123 <starpro123@naver.com>
2022-02-23 23:46:12 +0900

    setUp 호출하기

--- 2.PUnit/TestCase.py
+++ 2.PUnit/TestCase.py
@@ -3,9 +3,13 @@ class TestCase:
         self.name = name
     
     def run(self):
+        self.setUp()
         # 파이썬의 장점 클래스 이름이나 메서드 이름을 함수처럼 다룰 수 있다.
         method = getattr(self, self.name)
         method()
 
     def testMethod(self):
+        pass
+
+    def setUp(self):
         pass
--- 2.PUnit/WasRun.py
+++ 2.PUnit/WasRun.py
@@ -3,7 +3,12 @@ from TestCase import TestCase
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        TestCase.__init__(self, name)
+        TestCase.__init__(self, name)        
 
-    def testMethod(self):
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+
+    def run(self):
         self.wasRun = 1
+        self.wasSetUp = None
