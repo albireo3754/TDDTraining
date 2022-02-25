1e46a41c1999071014575cb1944b5c37308038bd
starpro123 <starpro123@naver.com>
2022-02-25 23:55:11 +0900

    SetUp을 구현하기

--- 2.PUnit/main.py
+++ 2.PUnit/main.py
@@ -3,22 +3,37 @@ class TestCase:
         self.name = name
 
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
+    def setUp(self):
+        pass
+
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = False
+        self.wasRun = 0
+        self.wasSetUp = 0
         TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.wasRun = 0
+        self.wasSetUp = 1
     
     def testMethod(self):
-        self.wasRun = True
+        self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
