be6904dec3e3c127d334c363e6d16c7424531d60
starpro123 <starpro123@naver.com>
2022-02-27 17:23:48 +0900

    Money 예제 초기화

--- 1.Money/src/main/java/Dollar.java
+++
@@ -1,9 +0,0 @@
-public class Dollar extends Money {
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-
-    public Money times(int multiplier) {
-        return new Dollar(amount * multiplier, currency);
-    }
-}
--- 1.Money/src/main/java/Franc.java
+++
@@ -1,9 +0,0 @@
-public class Franc extends Money {
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-
-    public Money times(int multiplier) {
-        return new Franc(amount * multiplier, currency);
-    }
-}
