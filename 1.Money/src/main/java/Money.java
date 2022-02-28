86e38c6c8514873d89055284e87e3d57dbb308ac
starpro123 <starpro123@naver.com>
2022-02-28 23:39:35 +0900

    작은 테스트를 만들고 필요한 계산에 대한 메타포를 신중하게 고려하고 테스트를 작성했다.

--- 1.Money/src/main/java/Money.java
+++ 1.Money/src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -26,5 +26,9 @@ class Money {
 
     static Money franc(int amount) {
         return new Money(amount, "CHF");
+    }
+
+    Money plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
