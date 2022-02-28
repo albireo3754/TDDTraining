4d6e27b2190af5edeb4a85a20b5a3da4a673c54c
starpro123 <starpro123@naver.com>
2022-02-28 22:37:25 +0900

    부모 객체를 추상타입으로 바꿨고, 팩토리 패턴으로 서브타입들을 생산했음

--- 1.Money/src/main/java/Money.java
+++ 1.Money/src/main/java/Money.java
@@ -1,8 +1,19 @@
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
+        System.out.println(money.amount);
         return amount == money.amount;
+    }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
     }
 }
--- 1.Money/src/test/java/DollarTests.java
+++ 1.Money/src/test/java/DollarTests.java
@@ -5,21 +5,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DollarTests {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Dollar five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     public void testFrancMultiplication() {
         Franc franc = new Franc(5);
-        assertEquals(new Franc(10), franc.times(2));
-        assertEquals(new Franc(15), franc.times(3));
+        assertEquals(Money.franc(10), franc.times(2));
+        assertEquals(Money.franc(15), franc.times(3));
     }
 
     @Test
     public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertEquals(Money.franc(5), Money.franc(5));
+//        assertTrue(new Franc(5).equals(new Franc(6)));
+        assertNotEquals(Money.franc(5), Money.franc(6));
     }
 }
