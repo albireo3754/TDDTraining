e25098ec33d491fa33deb063c16ab7dfa7499ed9
starpro123 <starpro123@naver.com>
2022-03-04 22:07:46 +0900

    setUp 추가하기

--- 2.PUnit/main.py
+++ 2.PUnit/main.py
@@ -3,21 +3,37 @@ class TestCase:
         self.name = name
 
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+
+    def setUp(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = 0
+        self.wasSetUp = 0
         super(WasRun, self).__init__(name)
 
     def testMethod(self):
         self.wasRun = 1
 
+    def setUp(self):
+        self.wasSetUp = 1
+
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        assert(not self.test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
