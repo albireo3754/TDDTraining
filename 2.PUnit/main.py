ae756cc0791cc5af0e2fe0a91caf80fe530560bb
starpro123 <starpro123@naver.com>
2022-03-04 22:17:58 +0900

    가짜 구현을 구현하였고 실패하는 테스트를 만들어 놓았다.

--- 2.PUnit/main.py
+++ 2.PUnit/main.py
@@ -3,17 +3,20 @@ class TestCase:
         self.name = name
 
     def run(self):
+        test = TestResult()
+        test.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return test
 
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
-    
+
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = 0
@@ -27,8 +30,21 @@ class WasRun(TestCase):
         self.wasRun = 1
         self.log += "testMethod "
 
+    def testBrokenMethod(self):
+        raise Exception
+
     def tearDown(self):
         self.log += "tearDown "
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount += 1
+
+    def summary(self):
+        return f"{self.runCount} run, 0 failed"
 
 class TestCaseTest(TestCase):
     def setUp(self):
@@ -38,4 +54,17 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert("setUp testMethod tearDown " == self.test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
