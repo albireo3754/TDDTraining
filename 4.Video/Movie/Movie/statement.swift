7aa2e450f5d8e4322ed270f2b0795377e6b64937
starpro123 <starpro123@naver.com>
2022-03-12 00:35:02 +0900

    함수 추출 & 임시 이름 부여

--- 4.Video/Movie/Movie/statement.swift
+++ 4.Video/Movie/Movie/statement.swift
@@ -76,14 +76,21 @@ class Movie {
         return volumeCredits
     }
     
-    func statement(invoice: Invoice) -> String? {
+    func appleSauce(_ invoice: Invoice) -> Int {
         var totalAmount = 0
+        for performance in invoice.performances {
+            totalAmount += amountFor(performance: performance)!
+        }
+        return totalAmount
+    }
+    
+    func statement(invoice: Invoice) -> String? {
+        
         var result = "청구 내역 (고객명: \(invoice.customer))\n"
         for performance in invoice.performances {
             result += "\(playFor(performance).name): \(usd(Double(amountFor(performance: performance)!))) (\(performance.audience)석)\n"
-            totalAmount += amountFor(performance: performance)!
         }
-        result += "총액: \(usd(Double(totalAmount)))\n"
+        result += "총액: \(usd(Double(appleSauce(invoice))))\n"
         result += "적립 포인트: \(totalVolumeCredits(invoice))점\n"
         return result
     }
