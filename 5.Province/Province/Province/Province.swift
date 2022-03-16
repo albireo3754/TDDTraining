33bf119f0e6574b1f2221d87f3907ed8098bf525
starpro123 <starpro123@naver.com>
2022-03-17 00:42:29 +0900

    Province예제 테스트가 통과하는 첫 케이스 완성

--- 5.Province/Province/Province/Province.swift
+++ 5.Province/Province/Province/Province.swift
@@ -7,24 +7,67 @@
 
 import Foundation
 
-struct Province {
+class Province {
+    struct ProducerDto {
+        let name: String
+        let cost: Int
+        var production: Int
+        
+        init(name: String, cost: Int, production: Int) {
+            self.name = name
+            self.cost = cost
+            self.production = production
+        }
+    }
+    
+    internal init(name: String, producers: [ProducerDto], totalProduction: Int, demand: Int, price: Int) {
+        self.name = name
+        self.producers = []
+        self.totalProduction = totalProduction
+        self.demand = demand
+        self.price = price
+        producers.forEach {
+            addProducer(Producer(province: self, data: $0))
+        }
+    }
+    
     let name: String
-    let producers: [Producer]
-    let totalProduction: Int
+    var producers: [Producer]
+    var totalProduction: Int
     let demand: Int
     let price: Int
     
-    func addProducer() {
-        
+    func addProducer(_ producer: Producer) {
+        producers.append(producer)
+        totalProduction += producer.production
+    }
+    
+    var shortfall: Int {
+        demand - totalProduction
     }
 }
 
-struct Producer {
+class Producer {
+    internal init(province: Province, data: Province.ProducerDto) {
+        self.name = data.name
+        self.cost = data.cost
+        self.province = province
+        self.production = data.production
+    }
+    
     let name: String
     let cost: Int
-    let production: Int
+    var province: Province
+    var production: Int
+    
+    func setProduction(amountStr: String) {
+        guard let amount = Int(amountStr) else { return }
+        let newProduction = amount
+        province.totalProduction += newProduction - production
+        production = newProduction
+    }
 }
 
 func sampleProvinceData() -> Province {
-    Province(name: "Asia", producers: [Producer(name: "Byzantium", cost: 10, production:9), Producer(name: "Attalia", cost: 12, production: 10), Producer(name: "Sinope", cost: 10, production: 6)], totalProduction: 0, demand: 30, price: 20)
+    Province(name: "Asia", producers: [Province.ProducerDto(name: "Byzantium", cost: 10, production:9), Province.ProducerDto(name: "Attalia", cost: 12, production: 10), Province.ProducerDto(name: "Sinope", cost: 10, production: 6)], totalProduction: 0, demand: 30, price: 20)
 }
