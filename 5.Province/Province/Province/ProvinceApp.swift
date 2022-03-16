8dd192ec9f6785e0e66202290d385c0056139c73
starpro123 <starpro123@naver.com>
2022-03-16 23:52:46 +0900

    Province예제 시작

---
+++ 5.Province/Province/Province/ProvinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProvinceApp.swift
+//  Province
+//
+//  Created by 윤상진 on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct ProvinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
