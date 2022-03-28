6e90d2cce0fcb12d49e9a8f9b67e75dcfacaf89a
starpro123 <starpro123@naver.com>
2022-03-29 01:55:58 +0900

    과감하게 테스트 추가와 리팩토링을 통해 currentFrame과 score를 추가 개선했다.

--- 7.Bowling/src/main/java/Game.java
+++ 7.Bowling/src/main/java/Game.java
@@ -2,21 +2,47 @@ public class Game {
     private int intsScore;
     private int itsCurrentThrow = 0;
     private int[] itsThrows = new int[21];
+    private int itsCurrentFrame = 1;
+    private boolean firstThrow = true;
 
     public void add(int pins) {
         intsScore += pins;
         itsThrows[itsCurrentThrow++] = pins;
+        adjustCurrentFrame();
+    }
+
+    private void adjustCurrentFrame() {
+        if (firstThrow) {
+            firstThrow = false;
+        } else {
+            firstThrow = true;
+            itsCurrentFrame++;
+        }
     }
 
     public int score() {
-        return intsScore;
+        return scoreForFrame(getCurrentFrame() - 1);
     }
 
-    public int scoreForFrame(int frame) {
+    public int scoreForFrame(int theFrame) {
+        int ball = 0;
         int score = 0;
-        for (int ball = 0; frame > 0 && (ball < itsCurrentThrow); ball += 2, frame--) {
-            score += itsThrows[ball] + itsThrows[ball + 1];
+        for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
+            int firstThrow = itsThrows[ball++];
+            int secondThrow = itsThrows[ball++];
+
+            int frameScore = firstThrow + secondThrow;
+            // 스페어는 다음 프레임의 첫 번째 투구에 필요하다.
+            if (frameScore == 10) {
+                score += frameScore + itsThrows[ball];
+            } else {
+                score += frameScore;
+            }
         }
         return score;
+    }
+
+    public int getCurrentFrame() {
+        return itsCurrentFrame;
     }
 }
--- 7.Bowling/src/test/java/TestGame.java
+++ 7.Bowling/src/test/java/TestGame.java
@@ -1,33 +1,61 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TestGame {
 
-    @Test
-    public void testOneThrow() {
-        Game g = new Game();
-        g.add(5);
-        assertThat(5).isEqualTo(g.score());
+    private Game g;
+
+    @BeforeEach
+    void setUp() {
+        g = new Game();
     }
+    // 프레임 중간에 score를 호출하는 경우는 고려하지 않는다.
+//    @Test
+//    public void testOneThrow() {
+//        g.add(5);
+//        assertThat(5).isEqualTo(g.score());
+//        assertThat(1).isEqualTo(g.getCurrentFrame());
+//    }
+
 
     @Test
     public void testTwoThrowsNoMark() {
-        Game g = new Game();
         g.add(5);
         g.add(4);
         assertThat(9).isEqualTo(g.score());
+        assertThat(2).isEqualTo(g.getCurrentFrame());
     }
 
     @Test
     public void testFourThrowsNoMark() {
-        Game g = new Game();
         g.add(5);
         g.add(4);
         g.add(7);
         g.add(2);
-        assertThat(18).isEqualTo(g.score());
         assertThat(9).isEqualTo(g.scoreForFrame(1));
         assertThat(18).isEqualTo(g.scoreForFrame(2));
+        assertThat(3).isEqualTo(g.getCurrentFrame());
+    }
+
+    @Test
+    public void testSimpleSpare() {
+        g.add(3);
+        g.add(7);
+        g.add(3);
+        assertThat(13).isEqualTo(g.scoreForFrame(1));
+        assertThat(2).isEqualTo(g.getCurrentFrame());
+    }
+
+    @Test
+    public void testSimpleFrameAfterSpare() {
+        g.add(3);
+        g.add(7);
+        g.add(3);
+        g.add(2);
+        assertThat(13).isEqualTo(g.scoreForFrame(1));
+        assertThat(18).isEqualTo(g.scoreForFrame(2));
+        assertThat(3).isEqualTo(g.getCurrentFrame());
     }
 }
