a28a33409144d8b2b39c5c150b7267747398280b
starpro123 <starpro123@naver.com>
2022-04-03 21:46:41 +0900

    Replace Temp with Query로 지역변수 제거하기

--- 8.Movie/Movie/Movie/Customer.swift
+++ 8.Movie/Movie/Movie/Customer.swift
@@ -24,20 +24,15 @@ class Customer {
         var frequentRenterPoints = 0
         var result = "Rental Record for \(name)\n"
         for rental in rentals {
-            let thisAmount = amountFor(rental)
             frequentRenterPoints += 1
             if rental.movie.priceCode == Movie.NEW_RELEASE && rental.daysRented > 1 {
                 frequentRenterPoints += 1
             }
-            result += "\t\(rental.movie.title)\t\(thisAmount)\n"
-            totalAmount += thisAmount
+            result += "\t\(rental.movie.title)\t\(rental.getCharge())\n"
+            totalAmount += rental.getCharge()
         }
         result += "Amount owed is \(totalAmount)\n"
         result += "You earned \(frequentRenterPoints) frequent renter points"
         return result
-    }
-    
-    private func amountFor(_ rental: Rental) -> Double {
-        return rental.getCharge()
     }
 }
