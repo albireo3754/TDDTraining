2542175b96338f6753a1b029a8f5bc5ead54fbd2
starpro123 <starpro123@naver.com>
2022-04-04 00:26:44 +0900

    Replacing Condintion Logic with Polymorphism을 하기 위해 사전 준비

--- 8.Movie/Movie/Movie/Movie.swift
+++ 8.Movie/Movie/Movie/Movie.swift
@@ -19,4 +19,33 @@ public class Movie {
         self.title = title
         self.priceCode = priceCode
     }
+    
+    public func getCharge(daysRented: Int) -> Double {
+        var result = 0.0
+        switch priceCode {
+        case Movie.REGULAR:
+            result += 2
+            if daysRented > 2 {
+                result += Double((daysRented - 2)) * 1.5
+            }
+        case Movie.NEW_RELEASE:
+            result += Double(daysRented * 3)
+        case Movie.CHILDRENS:
+            result += 1.5
+            if daysRented > 3 {
+                result += Double((daysRented - 3)) * 1.5
+            }
+        default:
+            break
+        }
+        return result
+    }
+    
+    public func getFrequentRenterPoints(daysRented: Int) -> Int {
+        var frequentRenterPoints = 1
+        if priceCode == Movie.NEW_RELEASE && daysRented > 1 {
+            frequentRenterPoints += 1
+        }
+        return frequentRenterPoints
+    }
 }
--- 8.Movie/Movie/Movie/Rental.swift
+++ 8.Movie/Movie/Movie/Rental.swift
@@ -17,31 +17,10 @@ public class Rental {
     }
     
     public func getCharge() -> Double {
-        var result = 0.0
-        switch movie.priceCode {
-        case Movie.REGULAR:
-            result += 2
-            if daysRented > 2 {
-                result += Double((daysRented - 2)) * 1.5
-            }
-        case Movie.NEW_RELEASE:
-            result += Double(daysRented * 3)
-        case Movie.CHILDRENS:
-            result += 1.5
-            if daysRented > 3 {
-                result += Double((daysRented - 3)) * 1.5
-            }
-        default:
-            break
-        }
-        return result
+        return movie.getCharge(daysRented: daysRented)
     }
     
     public func getFrequentRenterPoints() -> Int {
-        var frequentRenterPoints = 1
-        if movie.priceCode == Movie.NEW_RELEASE && daysRented > 1 {
-            frequentRenterPoints += 1
-        }
-        return frequentRenterPoints
+        return movie.getFrequentRenterPoints(daysRented: daysRented)
     }
 }
