60044f6058b2bb9d786cc3cefac4d0ec210ef8f5
starpro123 <starpro123@naver.com>
2022-04-11 23:46:58 +0900

    Move Field 리팩토링

--- 9.SwiftRefactoring/Refactoring/MovingFeaturesBetweenObjectTests/MoveFieldTests.swift
+++ 9.SwiftRefactoring/Refactoring/MovingFeaturesBetweenObjectTests/MoveFieldTests.swift
@@ -8,17 +8,30 @@
 import XCTest
 
 fileprivate enum AccountType {
-    case premium
-    case normal
+    case premium(Double)
+    case normal(Double)
+    
+    // 1. interestRate에 대한 변수를 Account에서 AccountType으로 옮겼다.
+    var interestRate: Double {
+        switch self {
+        case .premium(let rate):
+            return rate
+        case .normal(let rate):
+            return rate
+        }
+    }
 }
 
 fileprivate class Account {
     let type: AccountType
-    let interestRate: Double
     
-    init(type: AccountType, interestRate: Double) {
+    // 2. 자체 캡슐화 라고 표현을 했지만 extractMethod 로 봐도 될듯 -> 다시 옮기면 Inline Method임
+    private var interestRate: Double {
+        return type.interestRate
+    }
+    
+    init(type: AccountType) {
         self.type = type
-        self.interestRate = interestRate
     }
     
     func interestForAmountDays(amount: Double, days: Int) -> Double {
@@ -28,6 +41,6 @@ fileprivate class Account {
 
 class MoveFieldTests: XCTestCase {
     func test_interestForAmountDays() {
-        XCTAssertEqual(Account(type: .premium, interestRate: 0.5).interestForAmountDays(amount: 10, days: 365), 5)
+        XCTAssertEqual(Account(type: .premium(0.5)).interestForAmountDays(amount: 10, days: 365), 5)
     }
 }
