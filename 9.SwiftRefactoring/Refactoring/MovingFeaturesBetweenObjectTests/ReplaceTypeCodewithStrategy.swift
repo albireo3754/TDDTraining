5fae8578464817cfe4d6c0e0d94a69602b5f83d1
starpro123 <starpro123@naver.com>
2022-04-14 22:38:06 +0900

    Employee -> EmployeeType으로 코드 리팩토링

--- 9.SwiftRefactoring/Refactoring/MovingFeaturesBetweenObjectTests/ReplaceTypeCodewithStrategy.swift
+++ 9.SwiftRefactoring/Refactoring/MovingFeaturesBetweenObjectTests/ReplaceTypeCodewithStrategy.swift
@@ -8,9 +8,6 @@
 import XCTest
 
 fileprivate class Employee {
-    static let ENGINEER = 0
-    static let SALESMAN = 1
-    static let MANAGER = 2
     let type: Int
     
     init(type: Int) {
@@ -18,18 +15,75 @@ fileprivate class Employee {
     }
     
     func payAmount() -> Int {
+        // 1. EmployeeType으로 일단 코드 변경
         switch type {
-        case Employee.ENGINEER:
+        case Engineer.ENGINEER:
             return 10
-        case Employee.SALESMAN:
+        case Salesman.SALESMAN:
             return 10 + 10
-        case Employee.MANAGER:
+        case Manager.MANAGER:
             return 10 + 5
         default:
             return 0
         }
     }
 }
+
+fileprivate protocol EmployeeType {
+    static var ENGINEER: Int { get }
+    static var SALESMAN: Int { get }
+    static var MANAGER: Int { get }
+    
+    func getTypeCode() -> Int
+}
+
+fileprivate extension EmployeeType {
+    static var ENGINEER: Int { 0 }
+    static var SALESMAN: Int { 1 }
+    static var MANAGER: Int { 2 }
+    
+    static func newType(code: Int) -> EmployeeType {
+        switch code {
+        case ENGINEER:
+            return Engineer()
+        case SALESMAN:
+            return Salesman()
+        case MANAGER:
+            return Manager()
+        default:
+            fatalError()
+        }
+    }
+}
+
+fileprivate struct Engineer: EmployeeType {
+    func getTypeCode() -> Int {
+        return 0
+    }
+}
+
+fileprivate struct Salesman: EmployeeType {
+    func getTypeCode() -> Int {
+        return 1
+    }
+}
+
+fileprivate struct Manager: EmployeeType {
+    func getTypeCode() -> Int {
+        return 2
+    }
+}
+
+//fileprivate class EmployeeType {
+//    
+//    
+//    static func newType(code: Int) -> EmployeeType {
+//        switch code {
+//        case ENGINEER:
+//            return
+//        }
+//    }
+//}
 
 class ReplaceTypeCodewithStrategy: XCTestCase {
     func test_Employee() {
